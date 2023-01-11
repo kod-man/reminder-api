f86d42b0e5fa9c8fc5da6c17b10f8f11b55f405e
kodman <ufuk.kodman@gmail.com>
2023-01-11 22:09:46 +0100

    update me route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const User = require('../models/UserModel');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("../models/UserModel");
 
-const { validateUser } = require('../utils/validation');
-const { createToken } = require('../utils/commonMethods');
+const { validateUser } = require("../utils/validation");
+const { createToken } = require("../utils/commonMethods");
 
 const router = express.Router();
 
 // it will be /user/...
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   const { password, email } = req.body;
 
   try {
@@ -22,7 +22,7 @@ router.post('/register', async (req, res) => {
 
     // if user already exists
     if (findUser) {
-      res.status(400).json({ message: 'User already exists' });
+      res.status(400).json({ message: "User already exists" });
     }
 
     // hash the password
@@ -37,14 +37,14 @@ router.post('/register', async (req, res) => {
 
     await newUser.save();
 
-    return res.status(200).json({ message: 'User created succesfully' });
+    return res.status(200).json({ message: "User created succesfully" });
   } catch (error) {
-    return res.status(400).json({ message: 'Something went wrong.Please try again', error });
+    return res.status(400).json({ message: "Something went wrong.Please try again", error });
   }
 });
 
 // login route
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
@@ -58,14 +58,14 @@ router.post('/login', async (req, res) => {
 
     // if user doesn't exist
     if (!findUser) {
-      res.status(400).json({ message: 'Invalid email or password' });
+      res.status(400).json({ message: "Invalid email or password" });
     }
 
     // check if password is correct
     const isPasswordCorrect = bcrypt.compareSync(password, findUser.password);
 
     if (!isPasswordCorrect) {
-      res.status(400).json({ message: 'Invalid email or password' });
+      res.status(400).json({ message: "Invalid email or password" });
     }
 
     const token = await createToken(findUser._id);
@@ -75,32 +75,32 @@ router.post('/login', async (req, res) => {
         id: findUser._id,
       },
     };
-    return res.status(200).json({ message: 'Login successful', response });
+    return res.status(200).json({ message: "Login successful", response });
   } catch (error) {
-    return res.status(400).json({ message: 'Error logging in', error });
+    return res.status(400).json({ message: "Error logging in", error });
   }
 });
 
-router.get('/me', async (req, res) => {
+router.get("/me/:userId", async (req, res) => {
   try {
-    const user = await User.findById(req.body.id);
-    if (!user) return res.status(400).json({ message: 'User not found' });
+    const user = await User.findById(req.params.userId);
+    if (!user) return res.status(400).json({ message: "User not found" });
     return res.status(200).json({ user });
   } catch (error) {
-    return res.status(400).json({ message: 'Error getting user', error });
+    return res.status(400).json({ message: "Error getting user", error });
   }
 });
 
 // update user
-router.put('/username', async (req, res) => {
+router.put("/username", async (req, res) => {
   try {
     await User.findByIdAndUpdate(
       { _id: req.body.userId },
       { $set: { userName: req.body.userName, imageSrc: req.body.imageSrc } },
     );
-    res.status(200).json({ message: 'Succesfully updated' });
+    res.status(200).json({ message: "Succesfully updated" });
   } catch (error) {
-    res.status(400).json({ message: 'Error updating user', error });
+    res.status(400).json({ message: "Error updating user", error });
   }
 });
 
