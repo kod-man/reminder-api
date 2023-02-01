57dd6ab7f1aa78d653b6514581df2e832589d246
kodman <ufuk.kodman@gmail.com>
2023-02-01 16:59:57 +0100

    create project model

---
+++ models/ProjectModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const ProjectSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: false,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// export the model
+const Project = mongoose.model("Project", ProjectSchema);
+module.exports = Project;
