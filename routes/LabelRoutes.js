b8f37df300b0f2d091c1e47a12e4fc9667a39d07
Ekrl666 <ye.koral@gmail.com>
2023-03-05 00:08:14 +0300

    Add Label model and Label Route

---
+++ routes/LabelRoutes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const Label = require("../models/LabelModel");
+
+// const requireAuth = require('../utils/requireAuth');
+const router = express.Router();
+
+router.get("/all/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const labels = await Label.find({ userId });
+    return res.status(200).json(labels);
+  } catch (error) {
+    return res.status(400).json({ message: "Something went wrong", error });
+  }
+});
+
+router.post("/add", async (req, res) => {
+  const { name, color, userId, isFavorite } = req.body;
+  try {
+    const newLabel = new Label({
+      name,
+      color,
+      userId,
+      isFavorite,
+    });
+
+    await newLabel.save();
+    return res.status(200).json({ message: "Label added successfully" });
+  } catch (error) {
+    return res.status(400).json({ message: "Something went wrong", error });
+  }
+});
+module.exports = router;
